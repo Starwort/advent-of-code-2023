74afd3ba5d47af082d40962e8e56e137d960c38c
starwort <tcphone93@gmail.com>
2023-12-15 05:09:36 +0000

    121/35

--- day_15.py
+++ day_15.py
@@ -25,7 +25,16 @@ raw = aoc_helper.fetch(15, 2023)
 
 
 def parse_raw(raw: str):
-    return ...
+    return raw.split(",")
+
+
+def hash(data: str) -> int:
+    val = 0
+    for c in data:
+        val += ord(c)
+        val *= 17
+        val %= 256
+    return val
 
 
 data = parse_raw(raw)
@@ -35,7 +44,7 @@ data = parse_raw(raw)
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_one(data=data):
-    ...
+    return sum(map(hash, data))
 
 
 aoc_helper.lazy_test(day=15, year=2023, parse=parse_raw, solution=part_one)
@@ -45,7 +54,21 @@ aoc_helper.lazy_test(day=15, year=2023, parse=parse_raw, solution=part_one)
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_two(data=data):
-    ...
+    lenses = [{} for _ in range(256)]
+    for instruction in data:
+        if "=" in instruction:
+            focal_length = extract_ints(instruction)[-1]
+            lenses[hash(instruction.split("=")[0])][
+                instruction.split("=")[0]
+            ] = focal_length
+        if "-" in instruction:
+            if instruction[:-1] in lenses[hash(instruction[:-1])]:
+                del lenses[hash(instruction[:-1])][instruction[:-1]]
+    total = 0
+    for x, lens in enumerate(lenses, 1):
+        for i, focal_length in enumerate(lens.values(), 1):
+            total += i * focal_length * x
+    return total
 
 
 aoc_helper.lazy_test(day=15, year=2023, parse=parse_raw, solution=part_two)
