9bff73f53b31f25905e89023e0b78c76377784db
starwort <tcphone93@gmail.com>
2023-12-17 05:56:45 +0000

    Use new aoc_helper.Grid.pathfind implementation

--- day_17.py
+++ day_17.py
@@ -31,75 +31,38 @@ def parse_raw(raw: str):
 data = parse_raw(raw)
 
 
-def pathfind(
-    grid: "Grid[int]",
-    heuristic_multiplier: float = 1,
-    min_rx: int = 0,
-    max_rx: int = 3,
-) -> None | int:
-    to_visit = PrioQueue([(int(), int(), (int(), int()), int(), int(), [])])
-    visited = set()
-    target = len(grid.data[0]) - 1, len(grid.data) - 1
-
-    heuristic = lambda x, y: abs(x - target[0]) + abs(y - target[1])
-
-    for _heuristic_cost, cost, (x, y), rx, ry, history in to_visit:
+def next_state(max_rx: int = 3):
+    def next_state(
+        state: tuple[int, int], dx: int, dy: int, _prev_cell: int, _new_cell: int
+    ):
+        rx, ry = state
         if (
-            (x, y) == target
-            and (rx == 0 or abs(rx) >= min_rx)
-            and (ry == 0 or abs(ry) >= min_rx)
+            0 < rx < max_rx
+            and dx < 0
+            or -max_rx < rx < 0
+            and dx > 0
+            or abs(rx) == max_rx
+            and dx != 0
+            or 0 < ry < max_rx
+            and dy < 0
+            or -max_rx < ry < 0
+            and dy > 0
+            or abs(ry) == max_rx
+            and dy != 0
         ):
-            display = Grid(list(list(str(i) for i in row) for row in grid.data))
-            for x, y, rx, ry in history:
-                display[y][x] = (
-                    ">" if rx > 0 else "<" if rx < 0 else "^" if ry < 0 else "v"
-                )
-            print(display)
-            return cost
-        if (x, y, rx, ry) in visited:
-            continue
-        visited.add((x, y, rx, ry))
-        for neighbour, value in grid.orthogonal_neighbours(x, y):
-            if rx != 0 and rx < min_rx and rx > -min_rx:
-                # must continue in the same direction
-                if neighbour[0] == x:
-                    continue
-            if ry != 0 and ry < min_rx and ry > -min_rx:
-                if neighbour[1] == y:
-                    continue
-            # if neighbour x > x, rx must be positive or 0 and also less than 3
-            # if neighbour x < x, rx must be negative or 0 and also greater than -3
-            # if neighbour y > y, ry must be positive or 0 and also less than 3
-            # if neighbour y < y, ry must be negative or 0 and also greater than -3
-            if (
-                (neighbour[0] > x and (rx >= max_rx or rx < 0))
-                or (neighbour[0] < x and (rx <= -max_rx or rx > 0))
-                or (neighbour[1] > y and (ry >= max_rx or ry < 0))
-                or (neighbour[1] < y and (ry <= -max_rx or ry > 0))
-            ):
-                continue
-            next_cost = cost + value
-            new_rx = (rx + neighbour[0] - x) if neighbour[0] != x else 0
-            new_ry = (ry + neighbour[1] - y) if neighbour[1] != y else 0
-            to_visit.push(
-                (
-                    next_cost + heuristic(*neighbour) * heuristic_multiplier,
-                    next_cost,
-                    neighbour,
-                    # increment rx if was an x move, otherwise reset to 0
-                    new_rx,
-                    # increment ry if was a y move, otherwise reset to 0
-                    new_ry,
-                    [*history, (*neighbour, new_rx, new_ry)],
-                )
-            )
+            return None
+        return (rx * dx + dx, ry * dy + dy)
+
+    return next_state
 
 
 # providing this default is somewhat of a hack - there isn't any other way to
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_one(data=data):
-    return pathfind(data)
+    return data.pathfind(
+        initial_state=(0, 0), next_state=next_state(), cost_function=lambda _, j: j
+    )
 
 
 aoc_helper.lazy_test(day=17, year=2023, parse=parse_raw, solution=part_one)
@@ -109,7 +72,13 @@ aoc_helper.lazy_test(day=17, year=2023, parse=parse_raw, solution=part_one)
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_two(data=data):
-    return pathfind(data, min_rx=4, max_rx=10)
+    return data.pathfind(
+        initial_state=(0, 0),
+        next_state=next_state(10),
+        is_valid_end=lambda state: (abs(state[0]) >= 4 or state[0] == 0)
+        and (abs(state[1]) >= 4 or state[1] == 0),
+        cost_function=lambda _, j: j,
+    )
 
 
 aoc_helper.lazy_test(
