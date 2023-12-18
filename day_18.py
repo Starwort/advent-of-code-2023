e469e63edc4e8f43ce4f4b4bcdeb687f2a964dcd
starwort <tcphone93@gmail.com>
2023-12-18 05:40:06 +0000

    194/589

--- day_18.py
+++ day_18.py
@@ -1,4 +1,4 @@
-from collections import defaultdict, deque, Counter
+from collections import Counter, defaultdict, deque
 
 import aoc_helper
 from aoc_helper import (
@@ -15,8 +15,8 @@ from aoc_helper import (
     iter,
     list,
     map,
-    range,
     multirange,
+    range,
     search,
     tail_call,
 )
@@ -24,18 +24,60 @@ from aoc_helper import (
 raw = aoc_helper.fetch(18, 2023)
 
 
-def parse_raw(raw: str):
-    return ...
+def parse_raw(raw: str) -> list[tuple[tuple[int, int], int, tuple[int, int], int]]:
+    return (
+        list(raw.splitlines())
+        .mapped(str.split)
+        .starmapped(  # type: ignore
+            lambda dir, n, col: (
+                [(0, -1), (0, 1), (-1, 0), (1, 0)]["UDLR".index(dir)],  # type: ignore
+                int(n),
+                [(1, 0), (0, 1), (-1, 0), (0, -1)][int(col[-2])],
+                int(col[2:-2], 16),
+            )
+        )
+    )
 
 
 data = parse_raw(raw)
+
+
+def fill(grid: SparseGrid[bool], start: tuple[int, int]) -> int:
+    seen = set()
+    queue = deque([start])
+    filled = 0
+    while queue:
+        x, y = queue.popleft()
+        if (x, y) in seen:
+            continue
+        seen.add((x, y))
+        if (x, y) in grid.data:
+            continue
+        filled += 1
+        queue.extend((x + dx, y + dy) for dx, dy in [(0, -1), (0, 1), (-1, 0), (1, 0)])
+    return filled
+
+
+def shoelace(points: list[tuple[int, int]]) -> int:
+    total_area = 0
+    for p1, p2 in zip(points, points[1:] + points[:1]):
+        total_area += p1[0] * p2[1] - p1[1] * p2[0]
+    return abs(total_area // 2)
 
 
 # providing this default is somewhat of a hack - there isn't any other way to
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_one(data=data):
-    ...
+    x = y = int()
+    points = list([(x, y)])
+    total_walls = 0
+    for (dx, dy), n, *_ in data:
+        target = (x + dx * n, y + dy * n)
+        total_walls += n
+        points.append(target)
+        x, y = target
+    return shoelace(points) + total_walls // 2 + 1
 
 
 aoc_helper.lazy_test(day=18, year=2023, parse=parse_raw, solution=part_one)
@@ -45,7 +87,7 @@ aoc_helper.lazy_test(day=18, year=2023, parse=parse_raw, solution=part_one)
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_two(data=data):
-    ...
+    return part_one(data.starmapped(lambda a, b, c, d: (c, d, a, b)))
 
 
 aoc_helper.lazy_test(day=18, year=2023, parse=parse_raw, solution=part_two)
