f5ef7107ce00622463d3e99c79ee8942954d16d3
starwort <tcphone93@gmail.com>
2023-12-24 19:40:00 +0000

    Create Rust solution for day 23 Finishes in less than 2 seconds. Definitely should have done this the first time

---
+++ day_23/src/main.rs
@@ -0,0 +1,180 @@
+use std::collections::BinaryHeap;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum GridInfo {
+    Empty,
+    Wall,
+    SlopeUp,
+    SlopeDown,
+    SlopeLeft,
+    SlopeRight,
+}
+
+fn main() {
+    let mut grid = [[GridInfo::Empty; 141]; 141];
+    let input = raoc::sync_fetch(23, 2023, false);
+    for (y, line) in input.lines().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            grid[y][x] = match c {
+                '.' => GridInfo::Empty,
+                '#' => GridInfo::Wall,
+                '^' => GridInfo::SlopeUp,
+                'v' => GridInfo::SlopeDown,
+                '<' => GridInfo::SlopeLeft,
+                '>' => GridInfo::SlopeRight,
+                _ => panic!("Bad input: found '{}'", c),
+            };
+        }
+    }
+    let grid = grid;
+
+    let orthogonal_spaces = |x: usize, y: usize| {
+        let mut count = 0;
+        if x > 0 && grid[y][x - 1] != GridInfo::Wall {
+            count += 1;
+        }
+        if x < 140 && grid[y][x + 1] != GridInfo::Wall {
+            count += 1;
+        }
+        if y > 0 && grid[y - 1][x] != GridInfo::Wall {
+            count += 1;
+        }
+        if y < 140 && grid[y + 1][x] != GridInfo::Wall {
+            count += 1;
+        }
+        count
+    };
+
+    let mut nodes_of_interest = Vec::new();
+    for (y, row) in grid.iter().enumerate() {
+        for (x, &cell) in row.iter().enumerate() {
+            if cell == GridInfo::Empty && orthogonal_spaces(x, y) != 2 {
+                nodes_of_interest.push((x, y));
+            }
+        }
+    }
+
+    let mut paths = Vec::new();
+    for (i, &(x, y)) in nodes_of_interest.iter().enumerate() {
+        if i == 0 {
+            assert_eq!(y, 0);
+        } else if i == nodes_of_interest.len() - 1 {
+            assert_eq!(y, 140);
+        }
+        let mut outputs = Vec::new();
+        let mut todo = Vec::new();
+        for (dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+            let nx = x as isize + dx;
+            let ny = y as isize + dy;
+            if nx < 0 || nx > 140 || ny < 0 || ny > 140 {
+                continue;
+            }
+            todo.push((
+                nx as usize,
+                ny as usize,
+                1,
+                dx,
+                dy,
+                match grid[ny as usize][nx as usize] {
+                    GridInfo::Empty => false,
+                    GridInfo::SlopeUp => dy != -1,
+                    GridInfo::SlopeDown => dy != 1,
+                    GridInfo::SlopeLeft => dx != -1,
+                    GridInfo::SlopeRight => dx != 1,
+                    GridInfo::Wall => continue,
+                },
+            ));
+        }
+        while let Some((x, y, dist, dx, dy, p2_only)) = todo.pop() {
+            if let Some(i) = nodes_of_interest.iter().position(|&val| val == (x, y)) {
+                outputs.push((i, dist, p2_only));
+                continue;
+            }
+            for (dx, dy) in [(dx, dy), (dy, -dx), (-dy, dx)] {
+                let nx = x.wrapping_add_signed(dx);
+                let ny = y.wrapping_add_signed(dy);
+                if grid[ny][nx] != GridInfo::Wall {
+                    todo.push((nx, ny, dist + 1, dx, dy, p2_only));
+                }
+            }
+        }
+        outputs.sort_unstable_by_key(|&(_, dist, _)| dist);
+        outputs.reverse();
+        paths.push(outputs);
+    }
+    let paths = paths;
+    println!("{}", solve(&paths, false));
+    println!("{}", solve(&paths, true));
+}
+
+fn solve(paths: &[Vec<(usize, usize, bool)>], p2: bool) -> usize {
+    let mut todo = BinaryHeap::from([(
+        paths
+            .iter()
+            .map(|paths| {
+                paths
+                    .iter()
+                    .filter_map(
+                        |&(_, dist, p2_only)| {
+                            if p2 || !p2_only {
+                                Some(dist)
+                            } else {
+                                None
+                            }
+                        },
+                    )
+                    .max()
+                    .unwrap()
+            })
+            .sum::<usize>(), // heuristic
+        0usize, // distance
+        0usize, // current node
+        0u64,   // visited
+    )]);
+    let mut best = 0;
+
+    while let Some((_, dist, node, visited)) = todo.pop() {
+        let this_visit = 1 << node;
+        if visited & this_visit != 0 {
+            continue;
+        }
+        let best_possible = dist
+            + paths
+                .iter()
+                .enumerate()
+                .filter_map(|(i, paths)| {
+                    if visited & (1 << i) == 0 {
+                        paths
+                            .iter()
+                            .filter_map(|&(to, dist, p2_only)| {
+                                if (p2 || !p2_only) && visited & (1 << to) == 0 {
+                                    Some(dist)
+                                } else {
+                                    None
+                                }
+                            })
+                            .max()
+                    } else {
+                        None
+                    }
+                })
+                .sum::<usize>();
+        if best_possible <= best {
+            continue;
+        }
+        let visited = visited | this_visit;
+
+        if node == paths.len() - 1 {
+            best = best.max(dist);
+            continue;
+        }
+
+        for (next_node, next_dist, p2_only) in &paths[node] {
+            if (p2 || !p2_only) && visited & (1 << next_node) == 0 {
+                todo.push((dist + next_dist, dist + next_dist, *next_node, visited));
+            }
+        }
+    }
+
+    best
+}
