1c7f9ec8adc8a90ee93e9ee2a72170379ee6a6cf
starwort <tcphone93@gmail.com>
2024-01-09 23:32:48 +0000

    Further improve logic. 0.52 for both parts

--- day_23.py
+++ day_23.py
@@ -1,148 +1,12 @@
-from collections import Counter, defaultdict, deque
-from time import perf_counter
-
 import aoc_helper
-from aoc_helper import (
-    Grid,
-    PrioQueue,
-    SparseGrid,
-    decode_text,
-    extract_ints,
-    extract_iranges,
-    extract_ranges,
-    extract_uints,
-    frange,
-    irange,
-    iter,
-    list,
-    map,
-    multirange,
-    range,
-    search,
-    tail_call,
-)
+from aoc_helper import Grid, PrioQueue, list
 
 raw = aoc_helper.fetch(23, 2023)
 
 
-def parse_raw(raw: str):
-    return Grid.from_string(raw, str)
+def parse_raw(raw: str) -> tuple[list[tuple[int, int, bool]], list[int]]:
+    data = Grid.from_string(raw, str)
 
-
-data = parse_raw(raw)
-
-
-# providing this default is somewhat of a hack - there isn't any other way to
-# force type inference to happen, AFAIK - but this won't work with standard
-# collections (list, set, dict, tuple)
-def part_one(data=data):
-    pos = (data[0].index("."), int())
-    todo = PrioQueue([(int(), pos, frozenset())])
-    best = 0
-    for dist, (x, y), seen in todo:
-        if y < 0:
-            continue
-        if (x, y) in seen:
-            continue
-        if y == len(data.data) - 1:
-            best = max(best, -dist)
-            continue
-        match data[y][x]:
-            case "#":
-                continue
-            case "<":
-                todo.push((dist - 1, (x - 1, y), seen | {(x, y)}))
-            case ">":
-                todo.push((dist - 1, (x + 1, y), seen | {(x, y)}))
-            case "^":
-                todo.push((dist - 1, (x, y - 1), seen | {(x, y)}))
-            case "v":
-                todo.push((dist - 1, (x, y + 1), seen | {(x, y)}))
-            case ".":
-                for dx, dy in ((-1, 0), (1, 0), (0, 1), (0, -1)):
-                    todo.push((dist - 1, (x + dx, y + dy), seen | {(x, y)}))
-    assert best != 0
-    return best
-
-
-aoc_helper.lazy_test(day=23, year=2023, parse=parse_raw, solution=part_one)
-
-
-# providing this default is somewhat of a hack - there isn't any other way to
-# force type inference to happen, AFAIK - but this won't work with standard
-# collections (list, set, dict, tuple)
-# def part_two(data=data):
-#     pos = (data[0].index("."), int())
-#     tx, ty = (data[-1].index("."), len(data.data) - 1)
-#     todo = PrioQueue([(int(), int(), pos, frozenset())])
-#     best = 0
-#     for _heuristic, dist, (x, y), seen in todo:
-#         if y < 0:
-#             continue
-#         if (x, y) in seen:
-#             continue
-#         if data[y][x] == "#":
-#             continue
-#         else:
-#             PRUNE_VAL = 20
-#             HEURISTIC_VAL = 10
-#             if dist + (abs(x - tx) + abs(y - ty)) * PRUNE_VAL < best:
-#                 # probably hopeless
-#                 print(
-#                     "skipping",
-#                     dist,
-#                     (x, y),
-#                     # seen,
-#                     "as best is",
-#                     best,
-#                     "and heuristic is",
-#                     (abs(x - tx) + abs(y - ty)) * PRUNE_VAL,
-#                 )
-#                 continue
-#             if y == len(data.data) - 1:
-#                 best = max(best, dist)
-#                 continue
-#             for dx, dy in ((-1, 0), (1, 0), (0, 1), (0, -1)):
-#                 todo.push(
-#                     (
-#                         -dist - 1 - (abs(x - tx) - abs(y - ty)) * HEURISTIC_VAL,
-#                         dist + 1,
-#                         (x + dx, y + dy),
-#                         seen | {(x, y)},
-#                     )
-#                 )
-#     assert best != 0
-#     return best
-# def part_two(data=data):
-#     sx, sy = (data[0].index("."), int())
-#     tx, ty = (data[-1].index("."), len(data.data) - 1)
-#     todo = PrioQueue([(int(), int(), (tx, ty), frozenset())])
-#     best = 0
-#     for _heuristic, dist, (x, y), seen in todo:
-#         if (x, y) in seen:
-#             continue
-#         if data[y][x] == "#" or y > ty:
-#             continue
-#         elif y == 0:
-#             # best = max(best, dist)
-#             return dist
-#         else:
-#             for dx, dy in ((-1, 0), (1, 0), (0, 1), (0, -1)):
-#                 nx = x + dx
-#                 ny = y + dy
-#                 if ny > ty:
-#                     continue
-#                 todo.push(
-#                     (
-#                         -len(seen) - (abs(nx - sx) + abs(ny - sy)),
-#                         dist + 1,
-#                         (nx, ny),
-#                         seen | {(x, y)},
-#                     )
-#                 )
-#     assert best > 5638, best
-#     return best
-def part_two(data=data):
     sx, sy = (data[0].index("."), int())
     tx, ty = (data[-1].index("."), len(data.data) - 1)
     # first find nodes that are surrounded by 3 slopes
@@ -159,7 +23,7 @@ def part_two(data=data):
             ):
                 nodes_of_interest.append((x, y))
     # for each node, find the paths to the nearby nodes
-    paths = []
+    paths = list()
     for i, node in nodes_of_interest.enumerated():
         if i == 0:
             assert node == (sx, sy), f"{i=} {node=} {(sx, sy)=}"
@@ -179,7 +43,19 @@ def part_two(data=data):
             if y < 0 or y > ty:
                 continue
             if node in nodes_of_interest:
-                outputs.append((nodes_of_interest.index(node), dist))
+                outputs.append(
+                    (
+                        nodes_of_interest.index(node),
+                        dist,
+                        {
+                            ".": False,
+                            "^": dy != -1,
+                            "v": dy != 1,
+                            "<": dx != -1,
+                            ">": dx != 1,
+                        }[data[y][x]],
+                    )
+                )
                 continue
             for dx, dy in ((dx, dy), (dy, -dx), (-dy, dx)):
                 nx = x + dx
@@ -188,7 +64,19 @@ def part_two(data=data):
                     todo.append(((nx, ny), dist + 1, (dx, dy)))
         outputs.sort(key=lambda i: i[1])
         paths.append(outputs)
-    path_maxes = [max(i[1] for i in paths) for paths in paths]
+    path_maxes = list(max(i[1] for i in paths) for paths in paths)
+
+    return paths, path_maxes
+
+
+data = parse_raw(raw)
+
+
+# providing this default is somewhat of a hack - there isn't any other way to
+# force type inference to happen, AFAIK - but this won't work with standard
+# collections (list, set, dict, tuple)
+def part_one(data=data):
+    paths, path_maxes = data
     todo = PrioQueue(
         [
             (
@@ -216,7 +104,9 @@ def part_two(data=data):
 
         best_possible += path_maxes[node]
 
-        for next_pos, next_dist in paths[node]:
+        for next_pos, next_dist, p2_only in paths[node]:
+            if p2_only:
+                continue
             todo.push(
                 (
                     best_possible - next_dist,
@@ -225,7 +115,50 @@ def part_two(data=data):
                     visited,
                 )
             )
-    # assert best > 5638, best
+    return best
+
+
+aoc_helper.lazy_test(day=23, year=2023, parse=parse_raw, solution=part_one)
+
+
+def part_two(data=data):
+    paths, path_maxes = data
+    todo = PrioQueue(
+        [
+            (
+                -sum(path_maxes),
+                int(),
+                int(),
+                int(),
+            )
+        ]
+    )
+
+    best = 0
+    target = len(paths) - 1
+    for best_possible, dist, node, visited in todo:
+        this_visit = 1 << node
+        if visited & this_visit or best_possible < best:
+            continue
+        visited |= this_visit
+
+        if node == target:
+            if dist > best:
+                best = dist
+                print("-- best --", best)
+            continue
+
+        best_possible += path_maxes[node]
+
+        for next_pos, next_dist, _p2_only in paths[node]:
+            todo.push(
+                (
+                    best_possible - next_dist,
+                    dist + next_dist,
+                    next_pos,
+                    visited,
+                )
+            )
     return best
 
 
